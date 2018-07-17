146f3b19bca30f3c5adff97c9eb2ff63d50a3697
Michael Incardona <michael.incardona@outlook.com>
2018-07-17 19:05:38 -0400

    Add some documentation and a small refactor

--- AffinityWatcher/ProcessWatcher.cs
+++ AffinityWatcher/ProcessWatcher.cs
@@ -7,40 +7,82 @@ using System.Threading.Tasks;
 
 namespace AffinityWatcher
 {
+    /**
+     * Watches for processes and modifies their processor affinities.
+     */
     class ProcessWatcher
     {
+        // maps process names to their configurations
         Dictionary<string, WatchedProcessConfig> processes;
+        // WMI object that listens for new processes
+        /** @invariant watcher is null if and only if this object is stopped */
         ManagementEventWatcher watcher;
 
+        /**
+         * Creates a new ProcessWatcher with the supplied configuration.
+         * 
+         * Start() must be called to begin monitoring processes.
+         * 
+         * @param procConfigs the set of process configurations
+         */
         public ProcessWatcher(List<WatchedProcessConfig> procConfigs)
         {
             watcher = null;
+            // store the configurations in a dictionary for easy lookup by name
             processes = new Dictionary<string, WatchedProcessConfig>();
             foreach (var config in procConfigs) {
                 processes[config.ProcessName] = config;
             }
         }
 
+        /**
+         * Starts or restarts watching for new processes.
+         * 
+         * If this watcher is currently stopped, it is started. If it is
+         * currently started, it is stopped and then started again.
+         * 
+         * @throws Exception if the watcher cannot be started
+         */
         public void Start()
         {
             if (watcher != null) {
                 Stop();
             }
 
-            // these could throw
-            watcher = new ManagementEventWatcher(
-                new WqlEventQuery("SELECT * FROM Win32_ProcessStartTrace")
-            );
-            watcher.EventArrived += new EventArrivedEventHandler(ProcessStartHandler);
-            watcher.Start();
+            // try to create a new watcher
+            try {
+                // these could throw
+                watcher = new ManagementEventWatcher(
+                    new WqlEventQuery("SELECT * FROM Win32_ProcessStartTrace")
+                );
+                watcher.EventArrived += new EventArrivedEventHandler(ProcessStartHandler);
+                watcher.Start();
+            } catch {
+                watcher = null;
+                throw;
+            }
         }
 
+        /**
+         * Stops watching for processes.
+         * 
+         * @throws Exception if an error is signalled while stopping
+         * @post this object is stopped, even if an exception is thrown
+         */
         public void Stop()
         {
-            watcher.Stop();
-            watcher = null;
+            try {
+                watcher.Stop();
+            } catch {
+                throw;
+            } finally {
+                watcher = null;
+            }
         }
 
+        /**
+         * Event handler when a new process is noticed.
+         */
         private void ProcessStartHandler(object sender, EventArrivedEventArgs eventArgs)
         {
             PropertyDataCollection properties = eventArgs.NewEvent.Properties;
@@ -55,9 +97,11 @@ namespace AffinityWatcher
 
             //Console.WriteLine("Noticed process with name \"{0}\" and PID {1}", name, pid);
 
+            // apply any matching process config we have
             if (processes.TryGetValue(name, out WatchedProcessConfig processConfig)) {
                 Console.WriteLine("Process detected with name \"{0}\" and PID {1}", name, pid);
-
+                
+                // get the process
                 Process ps = null;
                 try {
                     // The WMI PID returned is a UInt32, but this factory method requires an int.
@@ -68,14 +112,10 @@ namespace AffinityWatcher
                     return;
                 }
 
+                // change the affinity
                 try {
                     ChangeProcessAffinity(ps, processConfig.TargetAffinity);
-                    var now = DateTime.Now;
-                    TimeSpan ts = new TimeSpan(0, 0, 0);
-                    if (now > timeOf) {
-                        ts = now.Subtract(timeOf);
-                    }
-                    Console.WriteLine("Changed affinity of process with PID {0} (delay {1}ms)", pid, ts.TotalMilliseconds);
+                    Console.WriteLine("Changed affinity of process with PID {0} (delay {1}ms)", pid, TimeSince(timeOf).TotalMilliseconds);
                 } catch (Exception e) {
                     Console.WriteLine("Unable to change affinity of process with PID {0}: {1}", pid, e.Message);
                     return;
@@ -83,6 +123,29 @@ namespace AffinityWatcher
             }
         }
 
+        /**
+         * Gets the time elapsed since some other time, clamped to be at least 0.
+         * 
+         * @param then the past time
+         * @return time elapsed since then, or a zero TimeSpan if then is now or in the future
+         */
+        private static TimeSpan TimeSince(DateTime then)
+        {
+            var now = DateTime.Now;
+            if (now > then) {
+                return now.Subtract(then);
+            } else {
+                return new TimeSpan(0, 0, 0);
+            }
+        }
+
+        /**
+         * Changes the affinity of a process.
+         * 
+         * @param ps the process to modify
+         * @param affinityMask mask for the new affinity
+         * @throws Exception if the affinity could not be changed
+         */
         private static void ChangeProcessAffinity(Process ps, long affinityMask)
         {
             var oldMask = (long)ps.ProcessorAffinity;
--- AffinityWatcher/WatchedProcessConfig.cs
+++ AffinityWatcher/WatchedProcessConfig.cs
@@ -6,11 +6,22 @@ using System.Threading.Tasks;
 
 namespace AffinityWatcher
 {
+    /**
+     * Configuration associated with a watched process.
+     */
     class WatchedProcessConfig
     {
+        /** The name of the process (e.g. "notepad.exe") */
         public string ProcessName { get; private set; }
+        /** The affinity mask to apply */
         public long TargetAffinity { get; private set; }
 
+        /**
+         * Constructs a new configuration object.
+         * 
+         * @param processName the name of the process
+         * @param targetAffinity the affinity mask
+         */
         public WatchedProcessConfig(string processName, long targetAffinity)
         {
             ProcessName = processName;
